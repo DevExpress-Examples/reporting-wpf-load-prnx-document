abbc62f0ebcc1e9c7b68aaa3df6c1a1bae2a7ffa
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to load the saved XtraReport document (*.prnx) in DocumentPreview 
+
+
+<p>This example illustrates how to load the saved XtraReport document (*.prnx) in the DocumentPreview control.</p>
+
+<br/>
+
+
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Printing
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim sl As New SimpleLink()
+			sl.PrintingSystem.LoadDocument("CustomerOrder.prnx")
+			documentPreview1.Model = New LinkPreviewModel(sl)
+		End Sub
+	End Class
+End Namespace
