4a80c350c75d7c437b89ab03c788c1ae6163e5ed
DevExpressExampleBot <fakeEmail>
2021-10-28 17:18:12 +0300

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,33 +1,23 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Printing
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim sl As New SimpleLink()
-			sl.PrintingSystem.LoadDocument("CustomerOrder.prnx")
-			documentPreview1.Model = New LinkPreviewModel(sl)
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim sl As SimpleLink = New SimpleLink()
+            sl.PrintingSystem.LoadDocument("CustomerOrder.prnx")
+            documentPreview1.Model = New LinkPreviewModel(sl)
+        End Sub
+    End Class
 End Namespace
